e5ff8bd0b2f2e4c88818d8daf720d5da8e9f5844
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2023-06-13 21:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+let urlText = "https://localhost:8080/";
+let button = document.getElementById("button");
+button.addEventListener("click",perform);
+
+function perform(){
+    let name = document.getElementById("name").value;
+    let year = document.getElementById("year").value;
+    let url =  document.getElementById("url");
+    if(name!=""){
+        url.innerText=urlText + `?name=${name}` + `&year=${year}`;
+    }
+    else{
+        url.innerText=urlText + `&year=${year}`;
+    }
+}   
